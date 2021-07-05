25ea076caff5488cba206e9ef08c90801cf08a7d
Daniel Zwelling <dzwellingmusic@gmail.com>
2021-07-05 15:15:37 -0700

    Fix build?

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ const port = process.env.PORT || 8080
 
 const router = express.Router();
 
-
-app.use(express.static(__dirname, 'dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
